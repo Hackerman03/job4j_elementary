a1d76a618a46f565a9881886bbabc20df9faa2ed
Hackerman03 <daviddduchovny@gmail.com>
2022-06-03 13:16:58 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Kirill Afanasiev");
+        System.out.println("13.06.1991");
+    }
+}
