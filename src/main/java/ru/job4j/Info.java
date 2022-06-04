fe994259018205c0d168c51bd4218d5a3647a537
Hackerman03 <daviddduchovny@gmail.com>
2022-06-04 14:53:27 +0300

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
-public class info {
+public class Info {
     public static void main(String[] args) {
-        System.out.println("03.06.2022");
+        System.out.println("04.06.2022");
     }
 }
